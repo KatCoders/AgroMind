bbf7e7a11590c5284dfddaa9cce3330e3ca67c0a
AgroMind <katcodersteam@gmail.com>
2025-10-06 11:39:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -699,8 +699,8 @@ with col1:
 with col2:
     components.html(
     """
-    <elevenlabs-convai agent-id="agent_3701k6p18w13ea6v401gdr3wpqsf"></elevenlabs-convai>
-    <script src="https://unpkg.com/@elevenlabs/convai-widget-embed" async type="text/javascript"></script>
+   <elevenlabs-convai agent-id="agent_9301k6w0hb3bf1zbf0pjd1wqwhex"></elevenlabs-convai>
+   <script src="https://unpkg.com/@elevenlabs/convai-widget-embed" async type="text/javascript"></script>
     """,
     height=400,  # जरूरत अनुसार बदल सकते हो
 )
@@ -948,5 +948,6 @@ st.markdown("""
     </small></p>
 </div>
 """, unsafe_allow_html=True)
+
 
 
